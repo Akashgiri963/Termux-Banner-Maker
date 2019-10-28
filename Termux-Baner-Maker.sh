8ff28460c9f117b3f987e7f5814431b04d5b71fe
Akashgiri963 <37062817+Akashgiri963@users.noreply.github.com>
2019-10-28 20:05:01 +0530

    Update Termux-Baner-Maker.sh

--- Termux-Baner-Maker.sh
+++ Termux-Baner-Maker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Sorry! This script is not completed yet..."
+echo -e "\n\n\nSorry! This script is not completed yet..."
 
-echo "Welcome to the Termux-Baner-Maker Window"
+echo -e "n\n\nWelcome to the Termux-Baner-Maker Window"
 
-echo -e "Enter the text for LOGO : \c" 
+echo -e "n\n\nEnter the text for LOGO : \c" 
 read text
 
-Echo "You have entered $text"
+echo "n\n\nYou have entered $text"
