9880aa676720046470f7c468bbc5de24379e479b
Akashgiri963 <37062817+Akashgiri963@users.noreply.github.com>
2019-10-28 19:57:04 +0530

    Create Termux-Baner-Maker.sh
    This is just a normal script . The main script is not written yet.
    Please wait for the original script
    
    
    
    Thank you

---
+++ Termux-Baner-Maker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Sorry! This script is not completed yet..."
+
+echo "Welcome to the Termux-Baner-Maker Window"
+
+echo -e "Enter the text for LOGO : \c" 
+read text
+
+Echo "You have entered $text"
