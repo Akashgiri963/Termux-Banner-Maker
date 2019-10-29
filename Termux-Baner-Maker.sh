9b9613a019165174718d8ede8d0d3c0399beb37a
Akashgiri963 <37062817+Akashgiri963@users.noreply.github.com>
2019-10-29 19:12:49 +0530

    Update Termux-Baner-Maker.sh
    Add New Style And Features

--- Termux-Baner-Maker.sh
+++ Termux-Baner-Maker.sh
@@ -1,14 +1,34 @@
 #! /bin/bash
 
-echo -e "\n\n\nWelcome to the Termux-Baner-Maker Window"
-echo "                          Code By : Akash Giri   "
-echo -e "\n\n\nEnter the text for LOGO : \c"
+echo ""
+echo "                 ______________________________________         "|lolc>
+echo "        ________|                                      |_______ "|lolc>
+echo "        \       |  WELCOME TO THE TERMUX BANER MAKER   |      / "|lolc>
+echo "         \      |                                      |     /  "|lolc>
+echo "         /      |______________________________________|     \  "|lolc>
+echo "        /__________)                                (_________\ "|lolc>
+echo ""
+
+figlet -f big Termux Banner| lolcat
+
+echo -e "\n\n\n\e[33mEnter The Text For LOGO : \e[32m\c"
 read text
+
+echo -e "\n\n\e[1m\e[33m\nWhat is Your Nick-Name : \e[32m\c"
+read name
 
 cd /data/data/com.termux/files/usr/etc
 
 echo "clear" > clear.txt
 echo "figlet -f slant "$text"|lolcat" > temp.txt
+echo "cowsay -f stegosaurus "$name"|lolcat" > name.txt
 
 cat clear.txt >> bash.bashrc
 cat temp.txt >> bash.bashrc
+cat name.txt >> bash.bashrc
+
+echo ""
+
+figlet -f slant "$text"|lolcat
+cowsay -f stegosaurus "$name"|lolcat
+figlet -f small "Thank You"|lolcat
