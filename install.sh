ce398937ddf8bef1f6f861e6aa8136ae450d1186
Akashgiri963 <37062817+Akashgiri963@users.noreply.github.com>
2019-10-29 10:51:13 +0530

    Create install.sh
    This is a requirement file which install requirement.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt update && apt upgrade
+apt install figlet -y
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+git clone https://github.com/xero/figlet-fonts
+cd /data/data/com.termux/files/usr/share
+mv figlet-fonts/* figlet && rm -rf figlet-fonts 
+figlet -f big Enjoy | lolcat
+echo
+echo
