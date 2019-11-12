036ac27d9a5fe7da82e2c23f2a9e88a9c7acfe87
4NONYM0U5 H4CK3R <37062817+Akashgiri963@users.noreply.github.com>
2019-11-12 14:47:04 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+echo -e "\n\n\e[33mUpdating Termux...\n\e[32m"
 apt update && apt upgrade
+echo -e "\n\n\e[33mDownloading Figlet...\n\e[32m"
 apt install figlet -y
+echo -e "\n\e[33mInstalling Toilet\n\e[32m"
 apt install toilet -y
+echo -e "\n\e[33mInstalling Cowsay\n\e[32m"
 apt install cowsay -y
+echo -e "\n\e[33mInstalling nano\n\e[32m"
 apt install nano -y
+echo -e "\n\e[33mInstalling Ruby\n\e[32m"
 apt install ruby -y
+echo -e "\n\e[33mInstalling Lolcat\n\e[32m"
 gem install lolcat
+echo -e "\n\e[33mDownloading Figlet-Fonts...\n\e[32m"
+cd /data/data/com.termux/files/home/Termux-Banner-Maker
 git clone https://github.com/xero/figlet-fonts
-cd /data/data/com.termux/files/usr/share
-mv figlet-fonts/* figlet && rm -rf figlet-fonts
+cp figlet-fonts/* /data/data/com.termux/files/usr/share/figlet
+rm -rf figlet-fonts
+echo -e "\n\e[33mSuccessfully Installed\n"
 chmod +x Termux-Baner-Maker.sh
 figlet -f big Enjoy | lolcat
-echo
-echo
+
+echo 'alias tbm="cd /data/data/com.termux/files/home/Termux-Banner-Maker;b>
+cat al.txt >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo -e "\n\e[36mType tbm anywhere in termux to run Termux-Banner-Maker\n\e[0m"
